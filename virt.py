8f2eaa98be199a36319035e08cfacb8432f092c4
Alexandre Kandalintsev <spam@messir.net>
2013-03-20 13:51:53 +0100

    virt.py: improved template

--- virt.py
+++ virt.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python3
 from kvmc import KVM, Bridged, Drive, CMD, kvms, main
 
-class Default(KVM):
+class Template(KVM):
+  name  = "template"
   mem   = 1024
-  cores = 1
-  net   = None
-  cmd   = "qemu-system-x86_64 --enable-kvm -curses"
-  auto  = True
-  template = True
-
+  net = [Bridged(ifname="virt%s"%i, model='e1000',
+         mac="52:54:91:5E:38:BB", br="intbr")]
+  drives = [Drive("/home/sources/perftests/arch64_template.qcow2", cache="unsafe")]
+  auto  = False
 
 for i in range(0, 8):
-  class MyKVM(Default):
-    name = "virt%s" % i
-    mem = 2048
+  class MyKVM(Template):
+    name = str(i)
     net = [Bridged(ifname="virt%s"%i, model='e1000',
            mac="52:54:91:5E:38:%02x"%i, br="intbr")]
     drives = [Drive("/home/sources/perftests/arch64_perf%s.qcow2"%i, cache="unsafe")]
 
-
 # to use from another scripts
 cmd = CMD(kvms)
-
 
 # if script is used stand-alone...
 if __name__ == '__main__':
