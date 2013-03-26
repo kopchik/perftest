a1f2858c88178291999fe3e702a1d0051d96c46c
Alexandre Kandalintsev <spam@messir.net>
2013-03-26 16:15:44 +0100

    virt.py: migrated to a new version of kvmc

--- virt.py
+++ virt.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-from kvmc import KVM, Bridged, Drive, CMD, kvms, main
+from kvmc import KVM, Bridged, Drive, manager, main
 
 class Template(KVM):
   name  = "template"
   mem   = 1024
   net = [Bridged(ifname="template", model='e1000',
          mac="52:54:91:5E:38:BB", br="intbr")]
-  drives = [Drive("/home/sources/perftests/arch64_template.qcow2", cache="unsafe")]
+  drives = [Drive("/home/sources/perftests/arch64_template.qcow2",
+            cache="unsafe")]
   auto  = False
 
 for i in range(0, 8):
@@ -14,10 +15,9 @@ for i in range(0, 8):
     name = str(i)
     net = [Bridged(ifname="virt%s"%i, model='e1000',
            mac="52:54:91:5E:38:%02x"%i, br="intbr")]
-    drives = [Drive("/home/sources/perftests/arch64_perf%s.qcow2"%i, cache="unsafe")]
+    drives = [Drive("/home/sources/perftests/arch64_perf%s.qcow2"%i,
+              cache="unsafe")]
 
-# to use from another scripts
-cmd = CMD(kvms)
 
 # if script is used stand-alone...
 if __name__ == '__main__':
