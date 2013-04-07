0366ff13e687658fa01949634c7972876d4c353f
Alexandre Kandalintsev <spam@messir.net>
2013-04-07 22:41:05 +0200

    perftool.py: it finally can count data

--- perftool.py
+++ perftool.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
+from useful.log import Log
 from subprocess import *
-from useful.log import log
+import termios
+import struct
+import fcntl
+import shlex
+import time
+import pty
 import os
 
 os.environ["PERF_PAGER"]="cat"
@@ -19,16 +25,36 @@ def get_events(hw=True, sw=True, cache=True, tracepoint=True):
   raw = check_output(cmd, shell=True)
   return raw.decode().strip(' \n').split('\n')
 
+
+def osexec(cmd):
+  cmd = shlex.split(cmd)
+  os.execlp(cmd[0], *cmd)
+
+
 def stat(pid, events, t):
   evcmd = ",".join(events)
   cmd = "sudo perf stat -e {events} --log-fd 1 -x, -p {pid}".format(events=evcmd, pid=pid)
-  p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+  log.info(cmd)
+  pid, fd = pty.fork()
+  if pid == 0:
+    osexec(cmd)
+  ## resise terminal
+  # fcntl.ioctl(fd, termios.TIOCSWINSZ, struct.pack("hhhh", 24, 80, 0, 0))
+  
+  #disable echo
+  flags = termios.tcgetattr(fd)
+  flags[3] &= ~termios.ECHO
+  termios.tcsetattr(fd, termios.TCSADRAIN, flags)
+
   time.sleep(t)
-  check_output("sudo kill -INT %s" % p.pid)
-  raw_data = p.stdout.readall()
-  log.notice(raw_data)
+  os.write(fd, b'\x03')
+  os.waitpid(pid, 0)  
+  raw_data = os.read(fd, 65535)
+  return raw_data.decode()
 
 
 if __name__ == '__main__':
-  print(get_events())
-  measure(pid=1,t=1)
+  print("You got the following counters in your CPU:\n",
+    get_events())
+  r = stat(pid=534,events=get_events(),t=1)
+  print(r)
