5ec3675bb4561f73dd02d482da984d317d88651c
Alexandre Kandalintsev <spam@messir.net>
2013-04-08 14:14:41 +0200

    perftool.py: imported PerfData code to parse 'perf stat' output

--- perftool.py
+++ perftool.py
@@ -10,6 +10,8 @@ import time
 import pty
 import os
 
+NOT_SUPPORTED = '<not supported>'
+NOT_COUNTED = '<not counted>'
 os.environ["PERF_PAGER"]="cat"
 log = Log("perftool")
 
@@ -38,8 +40,7 @@ def stat(pid, events, t):
   pid, fd = pty.fork()
   if pid == 0:
     osexec(cmd)
-  ## resise terminal
-  # fcntl.ioctl(fd, termios.TIOCSWINSZ, struct.pack("hhhh", 24, 80, 0, 0))
+  # fcntl.ioctl(fd, termios.TIOCSWINSZ, struct.pack("hhhh", 24, 80, 0, 0)) # resise terminal
   
   #disable echo
   flags = termios.tcgetattr(fd)
@@ -50,8 +51,76 @@ def stat(pid, events, t):
   ctrl_c = termios.tcgetattr(fd)[-1][termios.VINTR]  # get Ctrl+C character
   os.write(fd, ctrl_c)
   os.waitpid(pid, 0)  
-  raw_data = os.read(fd, 65535)
-  return raw_data.decode()
+  raw = os.read(fd, 65535)
+  return PerfData(raw)
+
+
+class PerfData(dict):
+  def __init__(self, rawdata):
+    super().__init__()
+
+    rawdata = rawdata.decode()
+    array = rawdata.split('\r\n')
+    if array[0].startswith("#"):
+      preamble = array.pop(0) #print("skipping preamble")
+    array = filter(lambda x: x, array) #filter out empty lines
+    array = map(lambda x: x.split(','), array)
+    for raw_value, key in array:
+      if raw_value == NOT_SUPPORTED:
+        value = False
+      elif raw_value == NOT_COUNTED:
+        value = None
+      else:
+        if raw_value.find('.') != -1:
+          value = float(raw_value)
+        else:
+          value = int(raw_value)
+
+      self[key] = value
+
+    if self.get('cycles', None):
+      self['cpu-cycles'] = self['cycles']
+    elif self.get('cpu-cycles', None):
+      self['cycles'] = self['cpu-cycles']
+
+    #instructions per cycle
+    inpc = None
+    if self.get('cycles', None)  and self.get('instructions', None):
+      cycles = self['cycles']
+      instructions = self['instructions']
+      if isinstance(cycles, int) and isinstance(instructions, int):
+        inpc = instructions / cycles
+    self['_inpc_'] = inpc
+
+
+  def normalized(self):
+    new_dict = {}
+
+    cycles = self['cycles']
+    for key, value in self.items():
+      if isinstance(value, int):
+        new_dict[key] = value/cycles
+      else:
+        new_dict[key] = value
+    return new_dict
+
+
+  def __repr__(self):
+    inpc = self['_inpc_']
+    if isinstance(inpc, float):
+      return "Perf(inpc={:.2f})".format(self['_inpc_'])
+    else:
+      return super().__repr__()
+
+
+def pperf(perf):
+  """pretty perf data printer"""
+  cycles = perf['cycles']
+  for key, value in perf.items():
+    if isinstance(value, int):
+      print(key, ":", value/cycles)
+    else:
+      print(key, value)
 
 
 if __name__ == '__main__':
