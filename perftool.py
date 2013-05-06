8d5689e8094b861dc574a5fb74f2bf83f5430174
Alexandre Kandalintsev <spam@messir.net>
2013-05-06 22:33:48 +0200

    added ann= and norm= parameters

--- perftool.py
+++ perftool.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from collections import OrderedDict
 from useful.log import Log
 from subprocess import *
 import termios
@@ -10,13 +11,15 @@ import time
 import pty
 import os
 
+
+counters_cmd = """perf list %s --no-pager |  grep -v 'List of' | awk '{print $1}' | grep -v '^$'"""
 NOT_SUPPORTED = '<not supported>'
 NOT_COUNTED = '<not counted>'
 os.environ["PERF_PAGER"]="cat"
 log = Log("perftool")
+BUF_SIZE = 65535
 
 
-counters_cmd = """perf list %s --no-pager |  grep -v 'List of' | awk '{print $1}' | grep -v '^$'"""
 def get_events(hw=True, sw=True, cache=True, tp=True):
   selector = ""
   if hw: selector += " hw"
@@ -46,7 +49,7 @@ def osexec(cmd):
   os.execlp(cmd[0], *cmd)
 
 
-def stat(pid, events, t):
+def stat(pid, events, t, ann=None, norm=False):
   evcmd = ",".join(events)
   cmd = "sudo perf stat -e {events} --log-fd 1 -x, -p {pid}".format(events=evcmd, pid=pid)
   pid, fd = pty.fork()
@@ -63,23 +66,27 @@ def stat(pid, events, t):
   ctrl_c = termios.tcgetattr(fd)[-1][termios.VINTR]  # get Ctrl+C character
   os.write(fd, ctrl_c)
   os.waitpid(pid, 0)
-  raw = os.read(fd, 65535)
-  return PerfData(raw)
+  raw = os.read(fd, BUF_SIZE)
+  assert len(raw) < BUF_SIZE, "buffer full on read"
+  return PerfData(raw, ann=ann, norm=norm)
 
 
-class PerfData(dict):
-  def __init__(self, rawdata):
+class PerfData(OrderedDict):
+  def __init__(self, rawdata, ann=None, norm=False):
     super().__init__()
+    self.ann  = ann   #annotation
+    self.norm = norm
 
-    rawdata = rawdata.decode()
-    array = rawdata.split('\r\n')
+    array = rawdata.decode().split('\r\n')
+    # skipping preamble
     if array[0].startswith("#"):
-      preamble = array.pop(0) #print("skipping preamble")
+      preamble = array.pop(0)
+    # skip empty lines and warnings
     for x in array:
       if not x or x.find('Warning:') != -1:
         array.remove(x)
-    #array = filter(lambda x: x, array) #filter out empty lines
     assert 'Not all events could be opened.' not in array
+    # convert raw values to int or float
     array = map(lambda x: x.split(','), array)
     for raw_value, key in array:
       if raw_value == NOT_SUPPORTED:
@@ -91,30 +98,39 @@ class PerfData(dict):
           value = float(raw_value)
         else:
           value = int(raw_value)
-
       self[key] = value
-
+    # ensure common data layout
     if self.get('cycles', None):
       self['cpu-cycles'] = self['cycles']
     elif self.get('cpu-cycles', None):
       self['cycles'] = self['cpu-cycles']
-
-    #instructions per cycle
+    #calculate instructions per cycle (inpc)
     inpc = None
-    if self.get('cycles', None)  and self.get('instructions', None):
+    if self.get('cycles', None) and self.get('instructions', None):
       cycles = self['cycles']
       instructions = self['instructions']
       if isinstance(cycles, int) and isinstance(instructions, int):
         inpc = instructions / cycles
     self['_inpc_'] = inpc
+    # normalize values if requested
+    if self.norm:
+      assert 'cycles' in self, "norm=True needs cycles to be measured"
+      cycles = self['cycles']
+      for k, v in self.items():
+        if isinstance(v, (int,float)):
+          self[k] = v/cycles
 
 
   def normalized(self):
-    new_dict = {}
-
+    if self.norm:
+      return self
+    assert 'cycles' in self, "no cycles counter"
+    new_dict = OrderedDict()
     cycles = self['cycles']
     for key, value in self.items():
-      if isinstance(value, int):
+      if key == '_inpc_':  # instructions-per-cycle is already normalized
+        new_dict[key] = value
+      if isinstance(value, (int,float)):
         new_dict[key] = value/cycles
       else:
         new_dict[key] = value
@@ -122,11 +138,20 @@ class PerfData(dict):
 
 
   def __repr__(self):
-    inpc = self['_inpc_']
-    if isinstance(inpc, float):
-      return "Perf(inpc={:.2f})".format(self['_inpc_'])
-    else:
-      return super().__repr__()
+    result = []
+    if self.ann:
+      result += ["ann=%s"%self.ann]
+    if self.norm:
+      result += ["norm=True"]
+    result += ["%s=%.4s"%(k,v) for k,v in self.items()]
+    return "Perf(%s)" % (", ".join(result))
+
+
+    # inpc = self['_inpc_']
+    # if isinstance(inpc, float):
+    #   return "Perf(inpc={:.2f})".format(self['_inpc_'])
+    # else:
+    #   return super().__repr__()
 
 
 def pperf(perf):
@@ -161,5 +186,5 @@ if __name__ == '__main__':
   # print("You got the following counters in your CPU:\n",
   #   get_events())
   print("making stats on own pid...")
-  r = stat(pid=os.getpid(), events=get_useful_events(), t=0.5)
+  r = stat(pid=os.getpid(), events=get_useful_events(), t=0.5, norm=True)
   print(r)
