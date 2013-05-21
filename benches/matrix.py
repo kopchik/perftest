976d3275bce4e6854a24710950c84a7edee24ef7
Alexandre Kandalintsev <spam@messir.net>
2013-05-21 15:05:45 +0200

    added matrix.py

---
+++ benches/matrix.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from useful.bench import StopWatch
+
+import numpy as np
+import gc; gc.disable()
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Run experiments')
+parser.add_argument('-d', '--debug', type=float, default=10, help="measurement time")
+parser.add_argument('-s', '--size', type=int, default=256, help="matrix size (N means NxN size)")
+parser.add_argument('-r', '--repeat', '--repeats', type=int, default=10, help="number of test repeats")
+args = parser.parse_args()
+
+t = StopWatch()
+
+A=np.random.randn(args.size, args.size)
+B=np.random.randn(args.size, args.size)
+#avg = Avg(report=repeats)
+#print("for matrix {0}x{0} ....".format(size))
+for y in range(args.repeat):
+    with StopWatch() as t:
+      np.dot(A, B)
+      result = t.stop()
+    print("Matrices {0}x{0}: real: {1:.2f} cpu: {2:.2f}".format(args.size, t.time, t.cpu))
+    #avg.append(result)
