783a643a986ddf6d37c671020afd48520b08b152
Alexandre Kandalintsev <spam@messir.net>
2013-05-27 23:21:17 +0200

    panda first benchmark

--- panda.py
+++ panda.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3
 from subprocess import Popen, PIPE
 from socket import socketpair
+from collections import OrderedDict
+from logging import basicConfig, DEBUG
 import shlex
 import gc
+
 BUFSIZE = 65535
+basicConfig(level=DEBUG)
 
 def parse(raw):
-  TODO
+  d = OrderedDict()
+  for l in raw.decode().splitlines():
+    v,k = l.split(',')
+    d[k] = v
+  return d
+
 
 def bench(cmd, cpu=None, evs=None, repeats=1):
   assert cpu is not None and evs, "provide CPU and events"
   if isinstance(evs, (list, tuple)): evs = ",".join(evs)
-  s1, s2 = socketpair()
+  me, other = socketpair()
   cmd = "schedtool -a {cpu} -e perf stat -e {evs} -x, --log-fd {fd} -r {repeats} -- {cmd}" \
-      .format(cpu=cpu, evs=evs, repeats=repeats, fd=s2.fileno(), cmd=cmd)
+      .format(cpu=cpu, evs=evs, repeats=repeats, fd=other.fileno(), cmd=cmd)
   print(cmd)
-  p = Popen(shlex.split(cmd), stderr=PIPE, shell=True)
+  p = Popen(shlex.split(cmd), stderr=PIPE, pass_fds=[other.fileno()])
+  
   r = p.wait()
   assert r == 0, "bad return code %s" % r
-  data = s2.recv(BUFSIZE)
-  assert len(data) < BUFSIZE, "data truncated"
-  print(data)
+  
+  raw = me.recv(BUFSIZE)
+  assert len(raw) < BUFSIZE, "data truncated"
+  return parse(raw)
+
 
 def main():
   gc.disable()
