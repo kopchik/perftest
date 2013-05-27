bf7910ff2cb7e6fe99a3ad4dc0035d8b5ce5b2c2
Alexandre Kandalintsev <spam@messir.net>
2013-05-27 23:49:07 +0200

    pyblosc.py: accept --repeats

--- benches/pyblosc.py
+++ benches/pyblosc.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 from useful.bench import StopWatch
 import numpy as np
+import argparse
 import blosc
 import sys
+
+parser = argparse.ArgumentParser(description='Run experiments')
+parser.add_argument('-r', '--repeat', '--repeats', type=int, default=100, help="number of repeats")
+args = parser.parse_args()
 
 a = np.linspace(0, 100, 3e6)
 bytes_array = a.tostring()
 
-for x in range(int(sys.argv[1])):
+for x in range(args.repeat):
   with StopWatch() as t:
     packed = blosc.pack_array(a)
     blosc.unpack_array(packed)
