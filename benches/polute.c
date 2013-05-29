1d0e1ce62787e196a4d0b01520f83bd1be35c872
Alexandre Kandalintsev <spam@messir.net>
2013-05-29 22:37:35 +0200

    polute.c: rework'd

--- benches/polute.c
+++ benches/polute.c
@@ -15,48 +15,47 @@ int main(int argc, char **argv) {
   int i,x,y;
   char mode[10];
   int *order;
-  int osize = 1*1024*1024;
   int *data;
   int dsize = 100*1024*1024;
   int cycles;
   int r;
 
   if (argc != 2) {
-    errx(2, "please specify dsize=%%d,osize=%%d,mode=%%s"); }
-  if ((r=sscanf(argv[1], "dsize=%d,osize=%d,cycles=%d,mode=%s", &dsize, &osize, &cycles, mode)) != 4) {
-    errx(4, "wrong parameters: read only %d chars", r); }
+    errx(2, "please specify dsize=%%d,cycles=%%d,mode=%%s"); }
+  if ((r=sscanf(argv[1], "dsize=%d,cycles=%d,mode=%s", &dsize, &cycles, mode)) != 3) {
+    errx(4, "wrong parameters: read only %d params", r); }
 
+  printf("dsize=%d,cycles=%d,mode=%s\n", dsize, cycles, mode);
+
+  if (cycles <= 0) {
+    errx(5, "cycles should be positive"); }
 
   if (dsize <= 0) {
     errx(5, "data array size should be positive"); }
-  printf("size of data array: %d\n", dsize);
   data = xcalloc(dsize, sizeof(int));
   for (i=0; i<dsize; i++) {
     data[i] = rand(); }
 
-  if (osize <= 0) {
-    errx(5, "order array size should be positive"); }
-  printf("size of order array: %d\n", osize);
-  order = xcalloc(osize, sizeof(int));
+
   if (strcmp(mode, "seq") == 0) {
-    for (i=0; i<osize; i++) {
-      order[i] = i % dsize; }}
+    for (int x=0; x<cycles; x++) {
+      for (i=0; i<dsize; i++) {
+        int val = data[i];}}}
   else if (strcmp(mode, "rand") == 0) {
+    order = xcalloc(dsize, sizeof(int));
     srand(time(NULL));
-    for (i=0; i<osize; i++) {
-      order[i] = rand() % dsize; }}
+    /* init order array */
+    for (i=0; i<dsize; i++) {
+      order[i] = rand() % dsize; }
+    /* traverse `cycles' times */
+    for (int x=0; x<cycles; x++) {
+      for (i=0; i<dsize; i++) {
+        int val = data[order[i]];}}}
   else {
-      errx(3, "unknown mode %s", mode);
-    }
-  printf("done\n");
+      errx(3, "unknown mode %s", mode);}
+  //printf("done\n");
 
 
-  for (int x=0; x<cycles; x++) {
-    for (i=0; i<osize; i++) {
-      x = data[order[i]];
-    }
-  printf("opa");
-  }
 
   return 0;
 }
