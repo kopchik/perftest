56f7da99f8590fc2b7fa5f13edbf0c8ccf7e5f77
Alexandre Kandalintsev <spam@messir.net>
2013-05-31 10:17:15 +0200

    stab.py: fixed loop iteration number

--- stab.py
+++ stab.py
@@ -13,7 +13,7 @@ CMD = "/usr/bin/time -f %e -o {out} -a {cmd}"
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Run experiments')
   parser.add_argument('-d', '--debug', default=False, const=True, action='store_const', help='enable debug mode')
-  parser.add_argument('-r', '--repeat', type=int, default=1000, help="how many times repeat each test")
+  parser.add_argument('-r', '--repeat', type=int, default=1002, help="how many times repeat each test")
   parser.add_argument('-s', '--sleep', type=float, default=0.3, help="interval between tests")
   parser.add_argument('-p', '--prefix', required=True, help="file prefix")
 
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     out = "results/{prefix}_{name}".format(prefix=args.prefix, name=name)
     cmd = CMD.format(cmd=cmd, out=out)
     cmd = shlex.split(cmd)
-    for x in range(1):
+    for x in range(args.repeat):
       cnt += 1
       if args.debug:
         print("runnig %s out of %s: %s" % (cnt, tot, cmd))
