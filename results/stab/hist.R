0c6cabad2453ef955326df26aada8ae7874b1097
Alexandre Kandalintsev <spam@messir.net>
2013-06-04 15:26:41 +0200

    fitting line

--- results/stab/hist.R
+++ results/stab/hist.R
@@ -5,17 +5,18 @@ my.plot <- function(files, output="plots.pdf", breaks="FD") {
   pdf(output)
   for (fname in files) {
     data <- read.csv(fname, header=T)[[1]]
-    hist(data, breaks=breaks, col='red', main=fname)
-    #xfit <- seq(min(data),max(data),length=40)
-    #yfit <- dnorm(xfit,mean=mean(data),sd=sd(data))
-    #yfit <- yfit*diff(h$mids[1:2])*length(data)
-    #lines(xfit, yfit, col="blue", lwd=2)
+    h <- hist(data, breaks=breaks, col='red', main=fname)
+    # fit
+    xfit <- seq(min(data),max(data),length=40)
+    yfit <- dnorm(xfit,mean=mean(data),sd=sd(data))
+    yfit <- yfit*diff(h$mids[1:2])*length(data)
+    lines(xfit, yfit, col="blue", lwd=2)
   }
+  dev.off()
 }
 
-my.plot(list.files(pattern="*s00*csv"), output="plots_00s.pdf")
-my.plot(list.files(pattern="*s03*csv"), output="plots_03s.pdf")
+my.plot(files=list.files(pattern="s0_.*csv"), output="plots_00s.pdf", breaks=20)
+my.plot(files=list.files(pattern="s03_.*csv"), output="plots_03s.pdf", breaks=20)
 
 
 #postscript("hist.pdf", horizontal=FALSE)
-#dev.off()
