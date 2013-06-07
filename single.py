45efd8a23793ae841d679991ec441655d52097dc
Alexandre Kandalintsev <spam@messir.net>
2013-06-07 16:55:22 +0200

    single.py: increased bencher.py number of spawned processes

--- single.py
+++ single.py
@@ -14,18 +14,18 @@ import time
 import gc
 
 benchmarks = dict(
-matrix = "bencher.py -s 100 -- /home/sources/kvmtests/benches/matrix 2048",
-integer = "bencher.py -s 100 -- /home/sources/kvmtests/benches/int",
+matrix = "bencher.py -s 100000 -- /home/sources/kvmtests/benches/matrix 2048",
+integer = "bencher.py -s 100000 -- /home/sources/kvmtests/benches/int",
 pgbench = "sudo -u postgres pgbench -c 20 -s 10 -T 100000",
-nginx_static = "siege -c 100 -t 666h http://localhost/big_static_file",  # TODO: too CPU consuming,
-wordpress = "siege -c 100 -t 666h http://localhost/",
-ffmpeg = "bencher.py -s 100 -- ffmpeg -i /home/sources/avatar_trailer.m2ts \
+nginx_static = "siege -c 10000 -t 666h http://localhost/big_static_file",  # TODO: too CPU consuming,
+wordpress = "siege -c 100000 -t 666h http://localhost/",
+ffmpeg = "bencher.py -s 100000 -- ffmpeg -i /home/sources/avatar_trailer.m2ts \
             -threads 1 -t 10 -y -strict -2 -loglevel panic \
             -acodec aac -aq 100 \
             -vcodec libx264 -preset fast -crf 22 \
             -f mp4 /dev/null",
-sdag  = "bencher.py -s 100 -- /home/sources/test_SDAG/test_sdag -t 5 -q 1000 /home/sources/test_SDAG/dataset.dat",
-sdagp = "bencher.py -s 100 -- /home/sources/test_SDAG/test_sdag+ -t 5 -q 1000 /home/sources/test_SDAG/dataset.dat",
+sdag  = "bencher.py -s 100000 -- /home/sources/test_SDAG/test_sdag -t 5 -q 1000 /home/sources/test_SDAG/dataset.dat",
+sdagp = "bencher.py -s 100000 -- /home/sources/test_SDAG/test_sdag+ -t 5 -q 1000 /home/sources/test_SDAG/dataset.dat",
 blosc = "/home/sources/kvmtests/benches/pyblosc.py -r 100000",
 )
 
