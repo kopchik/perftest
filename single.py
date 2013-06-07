1b58ef56141c8adff63f04ec74fe63d1bbbd1c27
Alexandre Kandalintsev <spam@messir.net>
2013-06-07 15:37:03 +0200

    single.py: rework to support double

--- single.py
+++ single.py
@@ -7,11 +7,11 @@ from perftool import get_useful_events
 from os.path import isdir
 from os import geteuid
 from virt import cgmgr
+from numa import *
 import argparse
 import rpyc
 import time
 import gc
-
 
 benchmarks = dict(
 matrix = "bencher.py -s 100 -- /home/sources/kvmtests/benches/matrix 2048",
@@ -30,6 +30,7 @@ blosc = "/home/sources/kvmtests/benches/pyblosc.py -r 100000",
 )
 
 
+PERF_CMD = "sudo perf kvm stat -e {events} -x, -p {pid} -o {output} sleep {t}"
 BOOT_TIME = 15
 WARMUP_TIME = 30
 MEASURE_TIME = 180
@@ -37,16 +38,17 @@ IDLENESS = 3
 
 events = get_useful_events()
 events = ",".join(events)
+log = getLogger(__name__)
 
-PERF_CMD = "sudo perf kvm stat -e {events} -x, -p {pid} -o {output} sleep {t}"
 
 
 def main():
-  log = getLogger(__name__)
   parser = argparse.ArgumentParser(description='Experiments with single tasks')
   parser.add_argument('--debug', default=False, const=True, action='store_const', help='enable debug mode')
   parser.add_argument('--prefix', required=True, help="prefix where to save the results")
+  parser.add_argument('-t', '--tests', default=['single', 'double'], nargs='*')
   args = parser.parse_args()
+  import sys; print(args); sys.exit();
 
   log.info(args)
   if geteuid() != 0:
@@ -57,6 +59,12 @@ def main():
   assert isdir(args.prefix), "prefix should be a valid directory"
 
   gc.disable()
+  if 'single' in args.tests:
+    single(args)
+  if 'double' in args.tests:
+    double(args)
+
+def single(args):
   with cgmgr:
     vm = cgmgr.start("0")
     time.sleep(BOOT_TIME)
@@ -64,6 +72,8 @@ def main():
     RPopen = rpc.root.Popen
     remains = len(benchmarks)
     for name, cmd in benchmarks.items():
+      print("remains %s tests" % remains)
+      remains -= 1
       output = args.prefix + '/' + name
       perf_cmd = PERF_CMD.format(pid=vm.pid, t=MEASURE_TIME, events=events, output=output)
 
@@ -79,6 +89,60 @@ def main():
       log.debug("finishing tests")
       p.killall()
       gc.collect()
+
+
+def double(prefix, far=False):
+  topology = OnlineCPUTopology()
+  if far:
+    cpu, bgcpu = topology.cpus_no_ht[:2]
+  else:
+    cpu = topology.cpus_no_ht[0]
+    bgcpu = topology.ht_siblings[cpu1][0]
+
+  with cgmgr:
+    vm = cgmgr.start(str(cpu))
+    bgvm = cgmgr.start(str(bgcpu))
+    time.sleep(BOOT_TIME)
+
+    rpc = retry(rpyc.connect, args=(str(vm.addr),), kwargs={"port":6666}, retries=10)
+    bgrpc = retry(rpyc.connect, args=(str(bgvm.addr),), kwargs={"port":6666}, retries=10)
+
+    RPopen = rpc.root.Popen
+    BGRPopen = bgrpc.root.Popen
+
+    remains = len(benchmarks)**2
+
+    for bgname, bgcmd in benchmarks.items():
+      log.debug("waiting for idleness")
+      wait_idleness(IDLENESS*2.3)
+      log.warning("launching %s in bg" % bgname)
+      bg = BGRPopen(bgcmd)
+      log.debug("warming up for %s" % WARMUP_TIME)
+      time.sleep(WARMUP_TIME)
+
+      for name, cmd in benchmarks.items():
+        print("remains %s tests" % remains)
+        remains -= 1
+        
+        outdir = args.prefix + '/' + bgname +'/'
+        try: os.makedirs(outdir)
+        except FileExistsError: pass
+
+        output =  outdir + name
+        perf_cmd = PERF_CMD.format(pid=vm.pid, t=MEASURE_TIME, events=events, output=output)
+        log.debug("starting %s" % name)
+        p = RPopen(cmd)
+        log.debug("warming up for %s" % WARMUP_TIME)
+        time.sleep(WARMUP_TIME)
+        log.debug("starting measurements")
+        run(perf_cmd)
+        assert p.poll() is None, "test unexpectedly terminated"
+        assert bg.poll() is None, "bg process suddenly died :("  
+        log.debug("finishing tests")
+        p.killall()
+        gc.collect()
+      bg.killall()
+      time.sleep(1)
 
 
 if __name__ == '__main__':
