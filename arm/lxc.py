9960d221fa35b59064db258fd0269d0f7d9a7217
Alexandre Kandalintsev <spam@messir.net>
2013-07-04 15:26:36 +0200

    separated lxc code

---
+++ arm/lxc.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+from useful.run import sudo, sudo_
+from useful.mstring import s
+from libvmc import gen_mac
+import os.path
+import os
+
+
+def asroot(f):
+  def wrapped(f):
+    pid = os.fork()
+    if pid:
+      os.waitpid(pid)
+    else:
+      os.seteuid(TODO)
+
+
+TPL = """
+lxc.utsname = ${self.name}
+lxc.rootfs = ${self.root}
+
+lxc.network.type = veth
+lxc.network.flags = up
+lxc.network.link = intbr
+lxc.network.hwaddr = ${self.mac}
+lxc.network.ipv4 = ${self.addr}
+lxc.network.name = eth0
+lxc.network.ipv4.gateway = ${self.gw}
+
+lxc.autodev = 1
+lxc.cap.drop = mknod
+lxc.tty = 4
+lxc.pts = 1024
+"""
+
+
+class LXC:
+  def __init__(self, name, root, tpl, addr, gw):
+    self.name = name
+    self.root = root
+    self.addr = addr
+    self.gw   = gw
+    self.tpl  = tpl
+    self.mac  = gen_mac()
+    self.started = False
+
+  def create(self):
+    if os.path.exists(self.root):
+      raise Exception(s("Cannot create snapshot: path exists: ${self.root}"))
+    sudo(s("btrfs subvolume snapshot ${self.tpl} ${self.root}"))
+    os.makedirs(s("/var/lib/lxc/${self.name}/"))
+    with open(s("/var/lib/lxc/${self.name}/config"), 'w') as fd:
+      data = s(TPL)
+      fd.write(data)
+
+  def destroy(self):
+    self.stop(t=1)
+    sudo_(s("lxc-destroy -n ${self.name}"))
+    if os.path.exists(self.root):
+      sudo(s("btrfs subvolume delete ${self.root}"))
+      sudo_(s("rm -rf ${self.root}"))
+
+  def start(self):
+    if self.started:
+      return
+    sudo(s("lxc-start -n ${self.name} -d"))
+
+  def stop(self, t=10):
+    sudo_(s("lxc-shutdown -n ${self.name} -t ${t}"))
--- arm/tests.py
+++ arm/tests.py
@@ -1,65 +1,29 @@
 #!/usr/bin/env python3
-from socket import gethostname
-from useful.mstring import s
-from subprocess import *
-from useful.run import *
 from ipaddress import IPv4Address
-import os.path
-import atexit
-import shlex
-import time
-import os
-
-
-HOSTNAME = gethostname()
-if HOSTNAME == "ux32vd":
-  PREFIX = "/mnt/btrfs/"
-elif HOSTNAME in ["odroid", "u2"]:
-  PREFIX = "/home"
-else:
-  raise Exception("Unknown host. Please add configuration for it.")
-
-
-class LXC:
-  def __init__(self, name, path, tpl, addr):
-    self.name = name
-    self.path = path
-    self.tpl = tpl
-    self.started = False
-
-  def create(self):
-    #lxc_tpl = "/home/exe/github/kvmtests/arm/configs/lxc-template.py"
-    lxc_tpl = "/home/sources/kvmtests/arm/configs/lxc-template.py"
-    if os.path.exists(path):
-      raise Exception(s("Cannot create snapshot: path exists: ${self.path}"))
-    sudo(s("btrfs subvolume snapshot ${self.tpl} ${self.path}"))
-    sudo(s("lxc-create -t ${lxc_tpl} -n ${self.name} -- --root ${self.path} --addr=${self.addr}"))
-
-  def destroy(self):
-    self.stop(t=1)
-    sudo_(s("lxc-destroy -n ${self.name}"))
-    if os.path.exists(self.path):
-      sudo(s("btrfs subvolume delete ${self.path}"))
-      sudo_(s("rm -rf ${self.path}"))
-
-  def start(self):
-    if self.started:
-      return
-    sudo(s("lxc-start -n ${self.name} -d"))
-
-  def stop(self, t=10):
-    sudo_(s("lxc-shutdown -n ${self.name} -t ${t}"))
+from socket import gethostname
+from lxc import LXC
 
 
 if __name__ == '__main__':
+  HOSTNAME = gethostname()
+  if HOSTNAME == "ux32vd":
+    PREFIX = "/mnt/btrfs/"
+  elif HOSTNAME in ["odroid", "u2"]:
+    PREFIX = "/home/"
+  else:
+    raise Exception("Unknown host. Please add configuration for it.")
+
   lxcs = []
   for x in range(4):
-    ip = str (IPv4Address("172.16.5.10")+x) + '/24'
+    ip = str(IPv4Address("172.16.5.10")+x) + '/24'
     print(ip)
-    lxc = LXC(name="perf0", path=PREFIX+"/perf0/", tpl=PREFIX+"/perftemplate/", addr="172.16.5.10/24")
+    name = "perf%s" % x
+    lxc = LXC(name=name, root=PREFIX+name, tpl=PREFIX+"/perftemplate/",
+              addr="172.16.5.10/24", gw="172.16.5.1")
     lxc.destroy()
     lxc.create()
-    lxc.start()
+    # lxc.start()
     lxcs += [lxc]
 
-  for lxc in lxcs: lxc.stop()
+  # for lxc in lxcs:
+  #   lxc.stop()
