a558ed7cfc2c31bd05e9fdd058c9a1d9cebdae4c
Alexandre Kandalintsev <spam@messir.net>
2013-07-05 14:57:02 +0200

    finishing reorg

---
+++ run_tests.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+print(__package__)
+from tests.test_odroid_u2 import *
---
+++ tests/u2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from ipaddress import IPv4Address
+from socket import gethostname
+from kvmtests.lxc import LXC
+
+
+if __name__ == '__main__':
+  HOSTNAME = gethostname()
+  if HOSTNAME == "ux32vd":
+    PREFIX = "/mnt/btrfs/"
+  elif HOSTNAME in ["odroid", "u2"]:
+    PREFIX = "/home/"
+  else:
+    raise Exception("Unknown host. Please add configuration for it.")
+
+  lxcs = []
+  for x in range(4):
+    ip = str(IPv4Address("172.16.5.10")+x) + '/24'
+    print(ip)
+    name = "perf%s" % x
+    lxc = LXC(name=name, root=PREFIX+name, tpl=PREFIX+"/perftemplate/",
+              addr="172.16.5.10/24", gw="172.16.5.1")
+    lxc.destroy()
+    #lxc.create()
+    lxcs += [lxc]
+    #lxc.start()
+
+  # for lxc in lxcs:
+  #   lxc.stop()
