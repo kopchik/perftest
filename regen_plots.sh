be10563f25b77bca826094d4ccb44f58dda93f51
Alexandre Kandalintsev <spam@messir.net>
2013-07-22 09:25:38 +0200

    regen_plots.sh: regen plots for all experiments

--- regen_plots.sh
+++ regen_plots.sh
@@ -2,10 +2,23 @@
 benches=('nginx' 'sdagp' 'integer' 'pgbench' 'matrix' 'ffmpeg' 'wordpress' 'blosc' 'sdag')
 
 
+regen() {
+src=$1
+dst=$2
+near=$3
 for bg in "${benches[@]}"; do
   for fg in "${benches[@]}"; do
-    mkdir -p "static/u2/${bg}/"
-    ./compare.py -p "results/u2/" -f $fg -b $bg -o "static/u2/${bg}/${fg}.png"
+    mkdir -p "${dst}/${bg}/"
+    if [ -n "$near" ]; then
+      ./compare.py -s -p $src -f $fg -b $bg -o "${dst}/${bg}/${fg}.png"
+    else
+      ./compare.py    -p $src -f $fg -b $bg -o "${dst}/${bg}/${fg}.png"
+    fi
   done
 done
+}
 
+regen "./results/panda/notp/" "./static/panda/"
+regen "./results/u2/" "./static/u2/"
+regen "./results/fx/cc_auto/notp/" "./static/fx_far/"
+regen "./results/fx/cc_auto/notp/" "./static/fx_near/" "near"
