c956ff5216cff47165479bcd5a9c73f99633f69e
Alexandre Kandalintsev <spam@messir.net>
2013-07-22 09:44:52 +0200

    config.py: added configuration for host p1

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ if HOSTNAME == "ux32vd":
   LXC_PREFIX = "/mnt/btrfs/"
 elif HOSTNAME in ["odroid", "u2"]:
   LXC_PREFIX = "/home/"
+elif HOSTNAME == 'p1':
+  LXC_PREFIX = "/mnt/btrfs/"
 else:
   raise Exception("Unknown host. Please add configuration for it.")
 
