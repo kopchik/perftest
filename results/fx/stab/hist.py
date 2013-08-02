dfc172e4ff047b9b29751e6d59f9cb9ab6c52f10
Alexandre Kandalintsev <spam@messir.net>
2013-08-02 13:05:14 +0200

    hist.py: small code improvements

--- results/fx/stab/hist.py
+++ results/fx/stab/hist.py
@@ -3,21 +3,41 @@ from pylab import *
 from scipy.stats import norm
 import sys
 
+def average(a):
+  return sum(a)/len(a)
+
+# from http://matplotlib.org/examples/pylab_examples/histogram_percent_demo.html
+@FuncFormatter
+def to_percent(y, position):
+  # Ignore the passed in position. This has the effect of scaling the default
+  # tick locations.
+  s = str(100 * y)
+
+  # The percent symbol needs escaping in latex
+  if matplotlib.rcParams['text.usetex'] == True:
+    return s + r'$\%$'
+  else:
+    return s + '%'
+
+
 COLOR = "#548BE3"
 def main(argv):
-  argv.pop(0)
-  if not argv:
-    sys.exit("please provide the path to file")
+  paths = argv[1:]
+  if not paths:
+    sys.exit("please specify files to plot")
   pltnum = len(argv)//2 + len(argv)%2
-  for i, fname in enumerate(argv):
+  for i, fname in enumerate(paths):
     with open(fname) as fd:
       values = []
       t = fd.readline().strip("#")
       for l in fd.readlines():
         values += [float(l.strip())]
-      subplot(pltnum,2, i+1)
+      subplot(pltnum, 2, i+1)
       title(t)
-      n, bins, patches = hist(values, bins=50, normed=True,
+      avg = average(values)
+      percents = map(lambda x: avg/x-1, values)
+      n, bins, patches = hist(list(percents),
+        bins=50, normed=True,
         histtype='bar', color=COLOR)
 
       mu, sigma = norm.fit(values)
@@ -25,14 +45,15 @@ def main(argv):
       plot(bins,y, 'r-', linewidth=3)
       #print("bins:", bins)
       #print("y:", y)
+
     ## remove y axis
     yaxis = gca().yaxis
     # yaxis.set_visible(False)
-
-
-  xaxis = gca().xaxis
-  # xaxis.set_major_formatter(to_percent)
-  xaxis.set_major_locator(MaxNLocator(nbins=5, prune='lower'))
+    
+    ## xaxis
+    xaxis = gca().xaxis
+    xaxis.set_major_formatter(to_percent)
+    xaxis.set_major_locator(MaxNLocator(nbins=5, prune='lower'))
 
   tight_layout(pad=0.5)
   savefig("/home/exe/github/perf2013paper/pic/stab_test.eps")
