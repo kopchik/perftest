03ce1317cb39097d0409a629f32fd1b18ad78342
Alexandre Kandalintsev <spam@messir.net>
2013-08-03 14:25:14 +0200

    article_plots.sh: more plots from the article

--- article_plots.sh
+++ article_plots.sh
@@ -3,3 +3,11 @@
 ./compare.py -p ./results/u2/ -b nginx -f matrix  --show -o ~/github/perf2013paper/pic/nginx_matrix.eps
 ./compare.py -p ./results/u2/ -b matrix -f sdag  --show -o ~/github/perf2013paper/pic/matrix_sdag.eps
 
+
+# STABILITY
+./stability_plot.py -f ./results/fx/stability/sleep_3/* \
+  -o ~/github/perf2013paper/pic/fx_stab_sleep_3.eps
+
+
+./stability_plot.py -f ./results/fx/stability/{sleep_0/sdagp,sleep_3/sdagp} \
+  -o ~/github/perf2013paper/pic/fx_stab_sleep3_vs_sleep_0.eps
