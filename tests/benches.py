d098c7c2b40eb2d0e0302e29e5c597c076d66cbb
Alexandre Kandalintsev <spam@messir.net>
2013-08-03 14:26:33 +0200

    benches.py: minor formatting corrections

--- tests/benches.py
+++ tests/benches.py
@@ -4,8 +4,8 @@ benches = dict(
 matrix  = "bencher.py -s 100000 -- /home/sources/perftest/benches/matrix 2048",
 integer = "bencher.py -s 100000 -- /home/sources/perftest/benches/int",
 pgbench = "sudo -u postgres pgbench -c 20 -s 10 -T 100000",
-nginx   = "siege -q -c 100 -t 666h http://localhost/big_static_file",  # TODO: too CPU consuming,
-wordpress = "siege -q -c 8 -t 666h http://localhost/",
+nginx   = "siege -q   -c 100 -t 666h http://localhost/big_static_file",  # TODO: too CPU consuming,
+wordpress = "siege -q -c 8   -t 666h http://localhost/",
 ffmpeg  = "bencher.py -s 100000 -- ffmpeg -i /home/sources/avatar_10s.m2ts \
             -threads 1 -t 10 -y -strict -2 -loglevel panic \
             -acodec aac -aq 100 \
