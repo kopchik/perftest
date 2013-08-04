73cff3b1644740f377b053389cdcdc898a9bae1c
Alexandre Kandalintsev <spam@messir.net>
2013-08-04 22:27:11 +0200

    regen_webplots.py: added ux32vd profile, added support for multi-core rendering

--- regen_webplots.py
+++ regen_webplots.py
@@ -2,9 +2,24 @@
 from tests.benches import benches
 from useful.mstring import s
 from plotter import perfbars
+import sys
 import os
 
-def regen(datadir, outputdir, sibling=True):
+pids = []
+def forkbg(f):
+  def wrapper(*args, **kwargs):
+    global pids
+    pid = os.fork()
+    if pid:
+      pids += [pid]
+      return
+    f(*args, **kwargs)
+    sys.exit("[%s] done" % os.getpid())
+  return wrapper
+
+
+@forkbg
+def regen(datadir, outputdir, sibling=True, quiet=True):
   for bg in benches:
     for fg in benches:
       try:
@@ -16,15 +31,25 @@ def regen(datadir, outputdir, sibling=True):
       single = s("${datadir}/single/${fg}")
       if sibling: double = s("${datadir}/double/${bg}/${fg}")
       else:       double = s("${datadir}/double_far/${bg}/${fg}")
-      print(out, single, double)
-      perfbars([single, double], output=out)
+      # print("perf bars params:", out, single, double)
+      perfbars([single, double], output=out, quiet=True)
 
 if __name__ == '__main__':
   print("u2")
   regen("results/u2/", "./static/u2/")
-  # print("fx far")
-  # regen("results/fx/cc_auto/notp/", "./static/fx_far/", sibling=False)
-  # print("fx near")
-  # regen("results/fx/cc_auto/notp/", "./static/fx_near/")
-  # print("panda")
-  # regen("results/panda/notp/", "./static/panda/")
+  print("fx far")
+  regen("results/fx/", "./static/fx_far/", sibling=False)
+  print("fx near")
+  regen("results/fx/", "./static/fx_near/")
+  #print("panda")
+  #regen("results/panda/notp/", "./static/panda/")
+  print("ux32vd")
+  regen("results/ux32vd", "./static/ux32vd/")
+
+
+  print("waiting workers to complete")
+  for p in pids:
+    try:
+      os.waitpid(p, 0)
+    except ChildProcessError:
+      pass
