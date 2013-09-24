d77f647fb64bd0de3eac6eeca9d99968bbcf66e4
Alexandre Kandalintsev <spam@messir.net>
2013-09-24 12:19:47 +0200

    table_ab.py: correlation with counters

--- table_ab.py
+++ table_ab.py
@@ -5,8 +5,11 @@ from operator import itemgetter
 import sys
 import os
 
-path_single = sys.argv[1]
-path_double = sys.argv[2]
+brutality = defaultdict(int)
+sensitivity = defaultdict(int)
+
+path_single = sys.argv[1] + '/'
+path_double = sys.argv[2] + '/'
 
 def sorted_dict(d):
   return sorted(d.items(), key=itemgetter(1), reverse=True)
@@ -32,20 +35,23 @@ def print_double(data1, data2):
     print(line)
 
 single = {}
+singlehpc = {}
 for name in os.listdir(path_single):
   p = csv2dict(path_single+name)
   inps = p['instructions'] / p['cycles']
   single[name] = inps
+  singlehpc[name] = p
 
-brutality = defaultdict(int)
-sensitivity = defaultdict(int)
+
 summary = 0
 double = {}
+doublehpc = {}
 print("~           &", " &".join(sorted(single)), "\\\\")
 for bg in sorted(single):
   print("{:12}".format(bg), end='')
   for fg in sorted(single):
     p = csv2dict(path_double+bg+'/'+fg)
+    doublehpc[bg,fg] = p
     inps = p['instructions'] / p['cycles']
     ratio = inps/single[fg]
     double[bg,fg] = ratio
@@ -79,3 +85,54 @@ print_double(brutality, sensitivity)
 # for k in key_order:
 #   v = sensitivity[k]
 #   print(k, fmt(v))
+
+def norm(d):
+  cycles = d['cycles']
+  for k,v in d.items():
+    d[k] = v/cycles
+  return d
+
+from scipy.stats.stats import pearsonr
+correlation = {}
+pvalue = {}
+for counter in singlehpc['sdag']:
+  bruts = []
+  stats = []
+  for (bname, brut) in sorted_dict(sensitivity):
+    bruts += [brut]
+    stats += [singlehpc[bname][counter]]
+  cor, p = pearsonr(bruts, stats)
+
+  if p < 0.05:
+    correlation[counter] = cor
+    pvalue[counter] = p
+for cnt, cor in sorted_dict(correlation):
+  print("{cnt:22} & {cor: 2.3f} & {pval:2.1%} \\\ \hline".format(cnt=cnt, cor=cor, pval=pvalue[cnt]))
+
+from collections import defaultdict
+from itertools import product
+degradations = []
+counters = defaultdict(list)
+
+print()
+for t2, t1 in product(singlehpc, repeat=2):
+  # print(t1, t2)
+  shpc1 = norm(singlehpc[t1])
+  shpc2 = norm(singlehpc[t2])
+  dhpc1 = norm(doublehpc[t1,t2])
+  dhpc2 = norm(doublehpc[t2,t1])
+
+  naive = shpc1['instructions'] + shpc2['instructions']
+  actual  = dhpc1['instructions'] + dhpc2['instructions']
+  degr = actual/naive
+  degradations += [degr]
+
+  for k, v1 in shpc1.items():
+    v2 = shpc2[k]
+    total = v1 + v2
+    counters[k] += [total]
+
+for counter,v in counters.items():
+  cor, p = pearsonr(v, degradations)
+  if p < 0.1:
+    print ("{:25} {: .3f} {:2.1%}".format(counter, cor, p*100))
