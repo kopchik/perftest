bbdf8c94ba8a4197ebcf74bd8f6adc83c197f46a
Alexandre Kandalintsev <spam@messir.net>
2014-03-05 15:03:51 +0000

    major fixes: check pid, fixed perf cmd line

--- experiment.py
+++ experiment.py
@@ -9,6 +9,7 @@ from config import basis, RESULTS, IDLENESS
 from utils import check_idleness, wait_idleness
 from subprocess import check_call
 import shlex
+import os
 
 
 if __name__ == '__main__':
@@ -20,18 +21,25 @@ if __name__ == '__main__':
   parser.add_argument('-i', '--interval', type=int, help="interval between measurements")
   args = parser.parse_args()
 
+
+  try:
+    os.kill(args.pid, 0)
+  except ProcessLookupError:
+    raise Exception("no such pid: %s" % args.pid)
+
   rpc = rpyc.connect(args.addr, port=6666)
   RPopen = rpc.root.Popen
-  PERFCMD = "sudo perf stat -I {interval} -e {events} -x, -o {output} sleep {time}"
+  PERFCMD = "sudo perf stat -I {interval} -e {events} -x, -o {output} -p {pid} sleep {time}"
   for name,cmd in basis.items():
     wait_idleness(IDLENESS)
-    print("launching %s (%s)"%(name,cmd))
+    print("launching %s"%name)
     if name != "pgbench":
       cmd = "bencher.py -s 100000 -- "+cmd
     p = RPopen(cmd)
 
-    perfcmd = PERFCMD.format(interval=args.interval, events="cycles:G", 
-                             output=RESULTS+name, time=args.time)
+    perfcmd = PERFCMD.format(interval=args.interval, events="cycles",
+                             output=RESULTS+name, time=args.time, pid=args.pid)
+    print(perfcmd)
     check_call(shlex.split(perfcmd))
 
     assert p.poll() is None, "test unexpectedly terminated"
